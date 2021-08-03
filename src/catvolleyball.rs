4152a8c02fd6a937cfb39e975756d30a459f0a8b
varun24143 <varun24143@gmail.com>
2021-08-03 17:42:24 +0530

    added component and player structs

--- src/catvolleyball.rs
+++ src/catvolleyball.rs
@@ -12,6 +12,27 @@ pub Enum side {
     Right,
 }
 
+pub struct Player {
+    pub side: Side,
+    pub width: f32,
+    pub height: f32,
+}
+
+impl Player {
+    fn new(side: Side) -> Player {
+        Player {
+            side,
+            width: PLAYER_WIDTH,
+            height: PLAYER_HEIGHT,
+        }
+    }
+}
+
+impl Component for Player {
+    type Storage = DenseVecStorage<self>;
+}
+
+// Initalizing the camera
 
 // Initialize the camera
 
