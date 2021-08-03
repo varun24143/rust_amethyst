6575e721d61a0a1013f8832f2044d5a351053583
varun24143 <varun24143@gmail.com>
2021-08-03 16:56:16 +0530

    initialization

---
+++ src/catvolleyball.rs
@@ -0,0 +1,26 @@
+use amethyst::{core::transform::Transform, prelude::*, renderer::Camera};
+
+pub const ARENA_HEIGHT: f32 = 500.0;
+pub const ARENA_WIDTH: f32 = 500.0;
+
+// Initialize the camera
+
+pub fn initialize_camera(world: &mut world) {
+    let mut transform = Transform::default();
+    transform.set_translation_xyz(ARENA_WIDTH * 0.5, ARENA_HEIGHT*0.5, 1);
+    world.create_entity()
+    .with(Camera::standard_2d(ARENA_WIDTH, ARENA_HEIGHT))
+    .with(transform)
+    .build();
+}
+
+pub struct CatVolleyball;
+
+impl SimpleState for CatVolleyball {
+
+    fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+        let world = data.world;
+        initialize_camera(world);
+    }
+
+}
